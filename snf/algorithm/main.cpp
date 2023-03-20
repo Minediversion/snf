b3b9c41e21f38f3031a148b86e78ce0b0b79a96a
marcr <marcrgbcn@gmail.com>
2023-03-20 20:38:40 +0100

    Added TODO tasks

--- snf/algorithm/main.cpp
+++ snf/algorithm/main.cpp
@@ -4,3 +4,10 @@ int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
+
+
+/*
+ * TODO: Set up a txt compression algorithm.
+ * TODO: Set up an app for the use of the .snf file.
+ * TODO: Set up an app for the creation of .snf from .txt.
+ * */
