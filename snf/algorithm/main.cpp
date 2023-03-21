b8cb46a3f17dd53441dc2258ab19c7ee65a9caf6
marcr <marcrgbcn@gmail.com>
2023-03-21 21:53:02 +0100

    Finished the simple text compression algorithm.

--- snf/algorithm/main.cpp
+++ snf/algorithm/main.cpp
@@ -6,25 +6,79 @@ bool isFileValid(std::string);
 
 namespace fs = std::filesystem;
 
-int main() {/*
+int main() {
+    // File preparation
+    /*
     std::string txtFilePath; // C:\Users\marcr\CLionProjects\snf\test.txt
-    std::getline(std::cin, txtFilePath); // Get file path*/
+    std::getline(std::cin, txtFilePath); // Get file path
+     */
     std::string txtFilePath = R"(C:\Users\marcr\CLionProjects\snf\test.txt)"; // Test file path
-    if(isFileValid(txtFilePath)){
-        std::size_t directory = txtFilePath.find_last_of('\\'); // Get last occurrence of \ to get directory
-        std::string snfFilePath = txtFilePath.substr(0, directory+1)
-                + txtFilePath.substr(directory+1, txtFilePath.length()-4-directory)
-                + "snf"; // Get the theoretical path were .snf would be
-        if(!fs::exists(snfFilePath)) std::ofstream snfFile (snfFilePath, std::ofstream::out); //Create file, opened for writing
-        else{
-            //Check if user wants to overwrite file
-            std::cout << "Do you want to overwrite existing .snf file? Y/N" << std::endl;
-            char c; std::cin >> c;
-            if(c == 'N') return 0;
-            fs::remove(snfFilePath); //Deletes previous file
-            std::ofstream snfFile (snfFilePath, std::ofstream::out); // Create file, opened for writing
+
+    if(!isFileValid(txtFilePath)) return 0; // Check if file is valid, if not en program
+
+    std::size_t directory = txtFilePath.find_last_of('\\'); // Get last occurrence of \ to get directory
+    std::string snfFilePath = txtFilePath.substr(0, directory+1)
+            + txtFilePath.substr(directory+1, txtFilePath.length()-4-directory)
+            + "snf"; // Get the theoretical path were .snf would be
+
+    if(fs::exists(snfFilePath)){ // Checks if there is already a compressed file
+        //Check if user wants to overwrite file
+        std::cout << "Do you want to overwrite existing .snf file? Y/N" << std::endl;
+        char c; std::cin >> c;
+        if(c == 'N') return 0;
+        fs::remove(snfFilePath); //Deletes previous file
+    }
+    std::fstream txtFile (txtFilePath, std::fstream::in); // Open file for reading
+    std::ofstream snfFile (snfFilePath, std::ofstream::out); // Create file, opened for writing
+
+    //------------------------------------------------------------------------------------------------------------------
+
+    // Classify words
+    std::string strBuf; // Create a buffer for the text file
+    std::unordered_map<std::string, std::size_t> words; // Create a map in order to classify words
+    while(txtFile){
+        txtFile >> strBuf; // Read from space to space of txtFile
+        // Delete non-alphanumeric characters from string classify
+        if(!isalpha(strBuf[strBuf.length()-1])) strBuf.erase(strBuf.length()-1, 1);
+        if(!isalpha(strBuf[0])) strBuf.erase(0, 1);
+        words[strBuf]++; // Count how many times strBuf appears in text
+    }
+    std::unordered_map<std::string, std::size_t> compWords; // Create vector to store words that appear more than once
+    std::size_t id = 0;
+    for(auto & word : words){
+        if(word.second >= 2) compWords[word.first] = id, id++; // Increase size of vector to add words
+    }
+    words.clear(); // Free up memory
+
+    //------------------------------------------------------------------------------------------------------------------
+
+    // Build compressed file
+    for(auto & word : compWords){
+        snfFile << std::to_string(word.second) << word.first << std::endl;
+    }
+    snfFile << "*-*-*" << std::endl;
+
+    strBuf = "";
+    txtFile.close();
+    txtFile.open(txtFilePath, std::fstream::in);
+    while(txtFile){
+        char nextChar = (char)txtFile.get();
+        if(!iswalnum(nextChar)){
+            if(compWords.find(strBuf) != compWords.end()){
+                snfFile << compWords[strBuf] << nextChar;
+            }else{
+                snfFile << strBuf << nextChar;
+            }
+            strBuf = "";
+        }else {
+            strBuf += nextChar;
         }
     }
+
+    // CLOSE AND REMOVE FILE IN ORDER FOR EASIER TESTING
+    snfFile.close();
+    //fs::remove(snfFilePath);
+
     return 0;
 }
 
